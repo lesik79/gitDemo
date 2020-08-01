e4ccda44cc60c3539f0faef1eb674e651a490ec8
lesik79 <costevao@gmail.com>
2020-08-01 15:47:45 -0500

    update HappyB20.java

--- src/gitPractice/HappyB20.java
+++ src/gitPractice/HappyB20.java
@@ -5,5 +5,6 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is happy");
         System.out.println("very happy");
+        System.out.println("hhhhhhh");
     }
 }
